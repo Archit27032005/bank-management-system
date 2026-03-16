c3c09c42282c81120f7de7db8d8ef1f91b550f53
Archit Bairagi <architsbairagi@gmail.com>
2026-03-16 17:43:27 +0530

    a1

--- bank.py
+++ bank.py
@@ -3,10 +3,14 @@ import random
 class Bank:
     def __init__(self):
         dic_id={}
+
     def main(self):
         print("-----Bank mangment system-----")
         print("1.Created Account\n2.Deposit Money\n3.Withdrawal Money\n4.Balance Inquiry\n5.Account Details\n6.Exit")
-        op=int(input("Enter Your Option:"))
+        try:
+            op=int(input("Enter Your Option:"))
+        except ValueError:
+            print("enter a number")
         if op==1:
             print("a.Savings Account or b.Current Account")
             op_2=input("Enter your option:").lower()
@@ -22,5 +26,35 @@ class Bank:
             self.balance_inq()
         elif op==5:
             self.account_display()
-        else:
+        elif op==6:
+            print("thank you for using Bank System. Goodbye!")
             exit()
+        else:
+            print("Invaild option picked")
+    
+
+    def saving_account(self):
+        try:
+            name=input("enter your name:")
+        except NameError:
+            print("pls enter correct name")
+        try:
+            ph_no=int(input("enter the phone number"))
+        except ValueError:
+        #creating 10 digit number in string
+        # 0 to 9 digit to selected and it done more 10 time in for loop
+         star = ("".join([str(random.randint(0,9))for _ in range(0,10)]))
+        # converting string into intger
+        acc_no = int(star)
+        bal = int(input("Enter a amount:"))
+        if bal < 100:
+            print("less than 100 ")
+            exit()
+        else:
+                print("Account created")
+                print(f"name:{name}")
+                print(f"Account number:{acc_no}")
+                print(f"phone number{ph_no}")
+                print(f"Balance{bal}")
+        
+        Bank.dic_id={acc_no:{name,ph_no,bal}}
