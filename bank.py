89cdb9b1245ec82a22df808da5c483b90368ccca
Archit Bairagi <architsbairagi@gmail.com>
2026-03-20 23:07:21 +0530

    a3

--- bank.py
+++ bank.py
@@ -12,6 +12,7 @@ class Bank:
                 op=int(input("Enter Your Option:"))
             except ValueError:
                 print("enter a number")
+                continue
             if op==1:
                 print("a.Savings Account or b.Current Account")
                 op_2=input("Enter your option:").lower()
@@ -47,7 +48,10 @@ class Bank:
         star = ("".join([str(random.randint(0,9))for _ in range(0,10)]))
         # converting string into intger
         acc_no = int(star)
-        bal = int(input("Enter a amount:"))
+        try:
+            bal = int(input("Enter a amount:"))
+        except ValueError:
+            print("Invalid amount. Please enter a number.")
         if bal < 100:
             print("less than 100 ")
             return
@@ -58,4 +62,55 @@ class Bank:
                 print(f"phone number{ph_no}")
                 print(f"Balance{bal}")
         
-        self.Bank.dic_id[acc_no]={"name":name,"ph_no":ph_no,"bal":bal, "type":"saving account"}
+        self.dic_id[acc_no]={"name":name,"ph_no":ph_no,"bal":bal, "type":"saving account"}
+    
+    def current_account(self):
+        name=input("enter your name:")
+        try:
+            ph_no=int(input("enter your number:"))
+        except ValueError:
+            print("enter a number")
+            return
+        
+        #creating 10 digit number in string
+        # 0 to 9 digit to selected and it done more 10 time in for loop
+        star = ("".join([str(random.randint(0,9))for _ in range(0,10)]))
+        # converting string into intger
+        acc_no = int(star)
+        try:
+            bal = int(input("Enter a amount:"))
+        except ValueError:
+            print("Invalid amount. Please enter a number.")
+        if bal < 100:
+            print("less than 100 ")
+            return
+        else:
+                print("Account created")
+                print(f"name:{name}")
+                print(f"Account number:{acc_no}")
+                print(f"phone number{ph_no}")
+                print(f"Balance{bal}")
+
+        self.dic_id[acc_no]={"name":name,"ph_no":ph_no,"bal":bal, "type":"current account"}
+
+    def deposit(self):
+        try:
+            no=int(input("enter your account number:"))
+        except ValueError:
+            print("enter a correct account number")
+            return
+        if no in self.dic_id:
+            try:
+                ad=int(input("enter your amount:"))
+            except ValueError:
+                print("Invalid amount. Please enter a number.")
+                return
+            ad += self.dic_id[no]["bal"]
+            print(f"new Balance is:{self.dic_id[no]["bal"]}")
+        else: 
+            print("account not found")
+
+        
+        
+        
+            
